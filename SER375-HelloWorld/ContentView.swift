6b3b0df7452929ae2d318504a9fde4436ab9eeb8
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-07 10:31:12 -0500

    Initial Commit

---
+++ SER375-HelloWorld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SER375-HelloWorld
+//
+//  Created by Taylor Lea on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SER375-HelloWorld/SER375_HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SER375_HelloWorldApp.swift
+//  SER375-HelloWorld
+//
+//  Created by Taylor Lea on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SER375_HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
